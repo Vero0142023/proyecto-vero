a4c11eb1c02a4294b6ec26c3653e285983237e64
Vero0142024 <vero.alejandra014@gmail.com>
2023-12-16 16:51:05 -0300

    mi primer commit

---
+++ main/main/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'index.html')
+
+def base(request):
+    return render(request, 'base.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+
