d9f0a9fcaafe3f69f7d13727307b2afded230fe5
Vero0142024 <vero.alejandra014@gmail.com>
2023-12-16 22:30:58 -0300

    mas cambios en categoria

--- main/apps/posts/views.py
+++ main/apps/posts/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def cambio(request):
+    return render(request, 'cambio.html')
--- main/main/views.py
+++ main/main/views.py
@@ -9,4 +9,18 @@ def base(request):
 def about(request):
     return render(request, 'about.html')
 
+def energia(request):
+    return render(request, 'energia.html')
+
+def educacion(request):
+    return render(request, 'educacion.html')
+
+def economia(request):
+    return render(request, 'economia.html')
+
+def cambio(request):
+    return render(request, 'cambio.html')
+
+def contacto(request):
+    return render(request, 'contacto.html')
 
