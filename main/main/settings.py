fb99265aa49edd39a6432fb6b68132d38cc5ab1a
Vero0142024 <vero.alejandra014@gmail.com>
2023-12-19 17:12:45 -0300

    volvio el inicio

--- main/main/settings.py
+++ main/main/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'main.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': (os.path.join(os.path.dirname(BASE_DIR),'templates')),
+        'DIRS': (os.path.join(os.path.dirname(BASE_DIR),'main.templates')),
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -124,7 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIR = (os.path.join(os.path.dirname(BASE_DIR),'static'))
+STATICFILES_DIR = (os.path.join(os.path.dirname(BASE_DIR),'main.static.img'))
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
